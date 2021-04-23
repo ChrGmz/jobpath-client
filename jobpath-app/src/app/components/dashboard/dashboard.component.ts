5de390d8e0b9e6af563f6030c928f91f8bac1297
Deida <deidalopez1@gmail.com>
2021-04-23 11:11:58 -0700

    set up edit modal, but not fully functional

--- jobpath-app/src/app/components/dashboard/dashboard.component.ts
+++ jobpath-app/src/app/components/dashboard/dashboard.component.ts
@@ -8,6 +8,11 @@ import {
 import {ApplicationData} from '../../interfaces/applicationdata'; 
 import { InProgressData } from '../../interfaces/inprogressdata'; 
 import { OfferData } from '../../interfaces/offerdata'; 
+import { MatDialog } from '@angular/material/dialog';
+import { SigninDialogComponent } from '../signin-dialog/signin-dialog.component';
+
+import { SigninDialogData } from '../../interfaces/signindialogdata';
+import {EditApplicationDialogComponent} from '../edit-application-dialog/edit-application-dialog.component'
 
 @Component({
   selector: 'app-dashboard',
@@ -15,6 +20,8 @@ import { OfferData } from '../../interfaces/offerdata';
   styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  constructor(private dialog: MatDialog) {}
+
   applications: ApplicationData[] = [];
 
   inProgress: ApplicationData[] = [];
@@ -37,6 +44,34 @@ export class DashboardComponent implements OnInit {
         benefits: ['Vacation', 'insurance', 'sick-days'],
       },
     ];
+  }
+
+  editApplication(i: string): void {
+    const dialogRef = this.dialog.open(EditApplicationDialogComponent, {
+      width: '270px',
+      data: {},
+    });
+    // dialogRef
+    //   .afterClosed()
+    //   .subscribe((result: SigninDialogData | undefined) => {
+    //     if (result) {
+    //       this.authService.signin(result.email, result.password);
+    //     }
+    //   });
+  }
+
+  newApplication(i: string): void {
+    const dialogRef = this.dialog.open(EditApplicationDialogComponent, {
+      width: '270px',
+      data: {},
+    });
+    // dialogRef
+    //   .afterClosed()
+    //   .subscribe((result: SigninDialogData | undefined) => {
+    //     if (result) {
+    //       this.authService.signin(result.email, result.password);
+    //     }
+    //   });
   }
 
   drop(event: CdkDragDrop<ApplicationData[]>) {
